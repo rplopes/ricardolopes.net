94899a1edb8ab8026226317bbe263ba873a154fb
Ricardo Lopes <mail@ricardolopes.net>
2024-09-19 23:29:41 +0100

    Improve post fetching code and recreate og images when needed

--- scripts/regenerator/index.mjs
+++ scripts/regenerator/index.mjs
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url';
 import stream from 'stream';
 import { promisify } from 'node:util';
 import { exec } from 'node:child_process';
+import crypto from 'crypto';
 
 const pipeline = promisify(stream.pipeline);
 const execPromise = promisify(exec);
@@ -17,7 +18,7 @@ const __dirname = path.dirname(__filename);
 
 const RSS_URL = 'https://rplopes.hashnode.dev/rss.xml';
 const BLOG_DIR = path.join(__dirname, '..', '..', 'blog');
-const TEMPLATE_PATH = path.join(BLOG_DIR, 'no-surprises', 'index.html');
+const TEMPLATE_PATH = path.join(BLOG_DIR, 'overly-defensive-coding', 'index.html');
 
 async function fetchRSS(url) {
   const response = await fetch(url);
@@ -36,19 +37,48 @@ async function fetchBlogPostContent(url) {
   const response = await fetch(url);
   const html = await response.text();
   const $ = cheerio.load(html);
-  
+
   // Use the correct selector for the post content
   const content = $('#post-content-wrapper').html();
-  
+
+  if (!content) throw new Error(`Could not find content for post: ${url}`);
+
   // Extract the reading time
   const readingTime = $('span:contains("min read")').text().trim();
-  
-  if (!content) {
-    console.warn(`Could not find content for post: ${url}`);
-    return null;
-  }
-  
+
+  if (!readingTime) throw new Error(`Could not find reading time for post: ${url}`);
+
   return { content, readingTime };
+}
+
+async function generateOgImage(title, estimate, dir) {
+  // Create a hash of the title and estimate
+  const hash = crypto.createHash('sha256').update(`${title}${estimate}`).digest('hex').slice(0, 16);
+  const ogImageFilename = `${hash}.png`;
+  const ogImagePath = path.join(dir, ogImageFilename);
+
+  // Check if the image with this hash already exists
+  const imageExists = await fsPromises
+    .access(ogImagePath)
+    .then(() => true)
+    .catch(() => false);
+
+  if (!imageExists) {
+    try {
+      await execPromise(
+        `TITLE="${title}" ESTIMATE="${estimate}" OUTPUT_PATH="${ogImagePath}" node scripts/ogimage/index.mjs`,
+        {
+          cwd: path.join(__dirname, '..', '..'),
+        }
+      );
+
+      console.log(`Generated ${ogImageFilename} for: ${title}`);
+    } catch (error) {
+      console.error(`Error generating ${ogImageFilename} for ${title}:`, error);
+    }
+  }
+
+  return ogImageFilename;
 }
 
 async function createBlogPost(item, template) {
@@ -57,7 +87,7 @@ async function createBlogPost(item, template) {
   await fsPromises.mkdir(dir, { recursive: true });
 
   const postData = await fetchBlogPostContent(item.link);
-  
+
   if (!postData) {
     console.error(`Skipping post due to missing content: ${item.link}`);
     return;
@@ -73,25 +103,19 @@ async function createBlogPost(item, template) {
     const img = $(element);
     let src = img.attr('src');
     if (src && src.startsWith('https://cdn.hashnode.com')) {
-      // Remove query parameters from the URL
       src = src.split('?')[0];
-      
       const filename = path.basename(src);
       const localPath = `/blog/${slug}/${filename}`;
       const fullLocalPath = path.join(dir, filename);
-      
-      // Update image src to absolute path
       img.attr('src', `https://ricardolopes.net${localPath}`);
-      
-      // Add promise to download image
+      // Remove class attribute from image
+      img.removeAttr('class');
       imagePromises.push(downloadImage(src, fullLocalPath));
     }
   });
 
-  // Wait for all images to be downloaded
   await Promise.all(imagePromises);
 
-  // Update content with modified image links
   content = $.html();
 
   const $template = cheerio.load(template, { decodeEntities: false });
@@ -99,27 +123,28 @@ async function createBlogPost(item, template) {
   // Update title
   $template('title').text(`${item.title} | Ricardo Lopes`);
   $template('h1[itemprop="headline"]').text(item.title);
+  $template('meta[property="og:title"]').attr('content', item.title);
 
-  // Update meta description and og:description
+  // Update description
   $template('meta[name="description"]').attr('content', item.description);
   $template('meta[property="og:description"]').attr('content', item.description);
 
-  // Update canonical link, og:url, and itemprop="url"
+  // Update canonical link
   const canonicalUrl = `https://ricardolopes.net/blog/${slug}/`;
   $template('link[rel="canonical"]').attr('href', canonicalUrl);
   $template('meta[property="og:url"]').attr('content', canonicalUrl);
   $template('link[itemprop="url"]').attr('href', canonicalUrl);
 
-  // Update og:title
-  $template('meta[property="og:title"]').attr('content', item.title);
-
-  // Update og:image
-  $template('meta[property="og:image"]').attr('content', `${canonicalUrl}og.png`);
-
   // Update publication date
   const pubDate = new Date(item.pubDate);
   $template('time[itemprop="datePublished"]').attr('datetime', pubDate.toISOString().split('T')[0]);
-  $template('time[itemprop="datePublished"]').text(pubDate.toLocaleDateString('en-US', { day: '2-digit', month: 'short', year: 'numeric' }));
+  $template('time[itemprop="datePublished"]').text(
+    pubDate.toLocaleDateString('en-US', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+    })
+  );
 
   // Update reading time
   const readingTimeElement = $template('.post-meta li:last-child');
@@ -128,25 +153,17 @@ async function createBlogPost(item, template) {
   // Update content
   $template('div[itemprop="articleBody"]').html(content);
 
+  // Generate og.png
+  const estimate = readingTime.split(' ')[0]; // Extract the number from "X min read"
+  const ogImageFilename = await generateOgImage(item.title, estimate, dir);
+
+  // Update og:image
+  $template('meta[property="og:image"]').attr('content', `${canonicalUrl}${ogImageFilename}`);
+
   // Generate the final HTML without pretty formatting
   let html = $template.html({ decodeEntities: false });
 
   await fsPromises.writeFile(path.join(dir, 'index.html'), html, 'utf8');
-
-  // Generate og.png
-  const title = item.title;
-  const estimate = readingTime.split(' ')[0]; // Extract the number from "X min read"
-  const ogImagePath = path.join(dir, 'og.png');
-  
-  try {
-    await execPromise(`TITLE="${title}" ESTIMATE="${estimate}" OUTPUT_PATH="${ogImagePath}" node scripts/ogimage/index.mjs`, {
-      cwd: path.join(__dirname, '..', '..') // Adjust this path as needed
-    });
-    
-    console.log(`Generated og.png for: ${title}`);
-  } catch (error) {
-    console.error(`Error generating og.png for ${title}:`, error);
-  }
 }
 
 async function main() {
